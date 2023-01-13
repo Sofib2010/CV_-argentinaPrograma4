2be5a2cfa0b90216893b33ab0c3252e0b54a830a
SofiaER <89816454+Sofib2010@users.noreply.github.com>
2023-01-12 23:04:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('botonVer').addEventListener('click', function(){
+    document.getElementById('formContacto').style.display='block' ;
+});
+document.getElementById('botonNoVer').addEventListener('click', function(){
+    document.getElementById('formContacto').style.display='none' ;
+});
+
